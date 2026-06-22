748b2b05a9ee4ee6de4af6fc1b9815cb488c3b27
Alkibria <asifalkibria@gmail.com>
2026-06-22 10:43:09 +0600

    Add mathematical operations program

---
+++ mathematical operations.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+print("The sum of the two numbers is: ", a+b)
+print("The difference of the two numbers is: ", a-b)
+print("The product of the two numbers is: ", a*b)
+if b != 0:
+    print("The quotient of the two numbers is: ", a/b)  
+else:    print("Cannot divide by zero.")
+    
