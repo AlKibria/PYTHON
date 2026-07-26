818d184a4186d6d8026a8470238e5e102cd376ae
Alkibria <asifalkibria@gmail.com>
2026-07-26 14:34:32 +0600

    Add Python course files

---
+++ Palindrome.py
@@ -0,0 +1,13 @@
+def is_palindrome(s):
+    if s == s[::-1]:
+        return True
+    else:
+        return False
+
+
+word = input("Enter a string: ")
+
+if is_palindrome(word):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
---
+++ swap.py
@@ -0,0 +1,14 @@
+
+a = 10
+b = 20
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swapping
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
